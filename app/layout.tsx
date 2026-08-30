226a91b2d9662c3f450eb3343dfea1291d8ef033
Arulraj2001 <arulraj2001@gmail.com>
2026-08-30 07:41:00 +0530

    feat(analytics): add Microsoft Clarity tracking script (ya8yfjq1s7)

--- app/layout.tsx
+++ app/layout.tsx
@@ -111,6 +111,18 @@ export default function RootLayout({
             `,
           }}
         />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ya8yfjq1s7");
+            `,
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://api.anthropic.com" />
