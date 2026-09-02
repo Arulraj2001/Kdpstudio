e6d52371bda2028625fd17425331379ccf0871f7
Arulraj2001 <arulraj2001@gmail.com>
2026-09-02 20:26:05 +0530

    fix(layout): Import Sparkles icon in AppShell to fix ReferenceError

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Sparkles } from 'lucide-react';
 import { Sidebar } from './Sidebar';
 import { TopBar } from './TopBar';
 import { PageRoute, TrimSize } from '../../types';
